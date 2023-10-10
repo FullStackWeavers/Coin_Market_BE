e7d50475b798aa05515be05ab80c6a07097f50e5
Jayden Ju (주재훈) <jjdong9108@gmail.com>
2023-10-10 15:26:08 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
 import * as passport from 'passport';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -22,6 +23,7 @@ async function bootstrap() {
         maxAge: 600000,
       },
     }),
+    cookieParser(),
   );
   app.use(passport.initialize());
   app.use(passport.session());
