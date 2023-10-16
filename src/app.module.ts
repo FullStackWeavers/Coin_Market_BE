393eb56e2588dd5e83d458ee05363568d19544d8
qndrl1004 <qndrl1004@gmail.com>
2023-10-16 13:00:54 +0900

    coinview re

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -31,7 +29,6 @@ import { ChatGateway } from './chat/chat.gateway';
     BithumbModule,
     ChatGateway,
   ],
-  controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  providers: [ChatGateway],
 })
 export class AppModule {}
