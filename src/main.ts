9551af4eba4f63fda6912a387e04fb0ec5f0c95a
LKW9 <92284361+LKW9@users.noreply.github.com>
2023-10-30 10:22:05 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   // await app.listen(3000, '0.0.0.0');
-  //await app.listen(3000);
+  await app.listen(3000);
 }
 
 bootstrap();
