721b6895be0ae45fa741063002b3a66790263be0
qndrl1004 <qndrl1004@gmail.com>
2023-10-30 11:50:13 +0900

    backend 수정

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(
-    3000,
-    'https://port-0-coin-market-be-12fhqa2llob5p0if.sel5.cloudtype.app',
-  );
+  await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap();
